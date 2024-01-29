518032099eb842f8dfc3f9e8f09966fe63cc4d73
Mohammed Aldahmani <dahmani@bu.edu>
2024-01-28 21:16:48 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,36 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        
+        if isinstance(self.p2, Add):
+            val2 = Add.evaluate(self.p2,x)
+        elif isinstance(self.p2, Sub):
+            val2 = Sub.evaluate(self.p2,x)
+        elif isinstance(self.p2, Mul):
+            val2 = Mul.evaluate(self.p2,x)
+        elif isinstance(self.p2, Div):
+            val2 = Div.evaluate(self.p2,x)
+        elif isinstance(self.p2, X):
+            val2 = x
+        else :
+            val2 = self.p2.i   
+
+        if isinstance(self.p1, Add):
+            val1 = Add.evaluate(self.p1,x)
+        elif isinstance(self.p1, Sub):
+            val1 = Sub.evaluate(self.p1,x)
+        elif isinstance(self.p1, Mul):
+            val1 = Mul.evaluate(self.p1,x)
+        elif isinstance(self.p1, Div):
+            val1 = Div.evaluate(self.p1,x)
+        elif isinstance(self.p1, X):
+            val1 = x
+        else :
+            val1 = self.p1.i  
+        return int(val1) + int(val2)
+            
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,6 +63,35 @@ class Mul:
         if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        
+        if isinstance(self.p2, Add):
+            val2 = Add.evaluate(self.p2,x)
+        elif isinstance(self.p2, Sub):
+            val2 = Sub.evaluate(self.p2,x)
+        elif isinstance(self.p2, Mul):
+            val2 = Mul.evaluate(self.p2,x)
+        elif isinstance(self.p2, Div):
+            val2 = Div.evaluate(self.p2,x)
+        elif isinstance(self.p2, X):
+            val2 = x
+        else :
+            val2 = self.p2.i   
+
+        if isinstance(self.p1, Add):
+            val1 = Add.evaluate(self.p1,x)
+        elif isinstance(self.p1, Sub):
+            val1 = Sub.evaluate(self.p1,x)
+        elif isinstance(self.p1, Mul):
+            val1 = Mul.evaluate(self.p1,x)
+        elif isinstance(self.p1, Div):
+            val1 = Div.evaluate(self.p1,x)
+        elif isinstance(self.p1, X):
+            val1 = x
+        else :
+            val1 = self.p1.i  
+        return int(val1) * int(val2)
 
 
 class Sub:
@@ -42,6 +101,35 @@ class Sub:
     
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, x):
+        
+        if isinstance(self.p2, Add):
+            val2 = Add.evaluate(self.p2,x)
+        elif isinstance(self.p2, Sub):
+            val2 = Sub.evaluate(self.p2,x)
+        elif isinstance(self.p2, Mul):
+            val2 = Mul.evaluate(self.p2,x)
+        elif isinstance(self.p2, Div):
+            val2 = Div.evaluate(self.p2,x)
+        elif isinstance(self.p2, X):
+            val2 = x
+        else :
+            val2 = self.p2.i   
+
+        if isinstance(self.p1, Add):
+            val1 = Add.evaluate(self.p1,x)
+        elif isinstance(self.p1, Sub):
+            val1 = Sub.evaluate(self.p1,x)
+        elif isinstance(self.p1, Mul):
+            val1 = Mul.evaluate(self.p1,x)
+        elif isinstance(self.p1, Div):
+            val1 = Div.evaluate(self.p1,x)
+        elif isinstance(self.p1, X):
+            val1 = x
+        else :
+            val1 = self.p1.i  
+        return int(val1) - int(val2)
     
 class Div:
     def __init__(self, p1, p2):
@@ -56,7 +144,43 @@ class Div:
         if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
+    def evaluate(self, x):
+        
+        if isinstance(self.p2, Add):
+            val2 = Add.evaluate(self.p2,x)
+        elif isinstance(self.p2, Sub):
+            val2 = Sub.evaluate(self.p2,x)
+        elif isinstance(self.p2, Mul):
+            val2 = Mul.evaluate(self.p2,x)
+        elif isinstance(self.p2, Div):
+            val2 = Div.evaluate(self.p2,x)
+        elif isinstance(self.p2, X):
+            val2 = x
+        else :
+            val2 = self.p2.i   
+
+        if isinstance(self.p1, Add):
+            val1 = Add.evaluate(self.p1,x)
+        elif isinstance(self.p1, Sub):
+            val1 = Sub.evaluate(self.p1,x)
+        elif isinstance(self.p1, Mul):
+            val1 = Mul.evaluate(self.p1,x)
+        elif isinstance(self.p1, Div):
+            val1 = Div.evaluate(self.p1,x)
+        elif isinstance(self.p1, X):
+            val1 = x
+        else :
+            val1 = self.p1.i
+        
+        return  val1/val2
 
 
-poly = Sub( Sub( Int(4), Int(3)), Sub( X(), Div( Int(1), Sub( Mul(X(), X()), Int(1)))))
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
+
+poly = Add(Int(3), Mul(X(),X()))
+print(poly, " = " , poly.evaluate(3))
